3ad167462f6a7787f13a5cb7410bb0baca2ab5a9
David Goddard <David.50mg@gmail.com>
2021-08-28 20:02:18 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtCore import QTimer, QObject, Signal
+
+from time import strftime, localtime
+
+class Backend(QObject):
+    # Signals:
+    #Todo: Work on connecting qml to python
+    update = Signal(str, arguments=['time'])
+
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+
+    eng = QQmlApplicationEngine()
+    eng.quit.connect(app.quit)
+    eng.load('main.qml')
+
+    currentTime = strftime("%H:%M:%S", localtime())
+    eng.rootObjects()[0].setProperty('timer1', currentTime)
+
+    sys.exit(app.exec_())
